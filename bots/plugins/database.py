e014078ba44b4593625ede1fdb958cfb997e53b9
Alnyz <katro.coplax@gmail.com>
2019-04-02 10:19:10 +0700

    Update database.py

--- bots/plugins/database.py
+++ bots/plugins/database.py
@@ -3,12 +3,7 @@ import pymongo
 
 import traceback
 
-<<<<<<< HEAD
-URI =""
-=======
 URI ="Your mongodb URI"
->>>>>>> ac2a7d2f52a53edc3f181774201a4ddc03c6e5d2
-
 
 class DataBase(object):
 	def __init__(self):
@@ -70,13 +65,13 @@ class DataBase(object):
 					bots: bool = False,
 					groups: bool = False,
 					users: bool = False):
-<<<<<<< HEAD
+
 		if admin:return [i["_id"] for i in self.col.admin_db.find()]
 		elif bots:return [i["_id"] for i in self.col.bot_db.find()]
 		elif groups:return [i["_id"] for i in self.col.group_db.find()]
 		elif users:	return [i["_id"] for i in self.col.users_db.find()]
 				
-=======
+
 		if admin:			
 			return [i["_id"] for i in self.col.admin_db.find()]
 		elif bots:		
@@ -86,4 +81,3 @@ class DataBase(object):
 		elif users:			
 			return [i["_id"] for i in self.col.users_db.find()]
 				
->>>>>>> ac2a7d2f52a53edc3f181774201a4ddc03c6e5d2
