ca7b14c9bd0570982e7f8ec157f86375a43202fb
Alnyz <katro.coplax@gmail.com>
2019-04-18 20:42:02 +0700

    add more docs, adding regex filter, fix bug!

--- linepy/filters.py
+++ linepy/filters.py
@@ -1,3 +1,5 @@
+import re
+
 class Filter:
     def __call__(self, message):
         raise NotImplementedError
@@ -41,12 +43,13 @@ class OrFilter(Filter):
 def create(name: str, func: callable, **kwargs) -> type:
 	d = {"__call__": func}
 	d.update(kwargs)
+	
 	return type(name, (Filter,), d)()
-
+    
 class Filters:
-
+	
 	create = create
-
+	
 	#content
 	text = create("Text", lambda _,m: bool(m.contentType == 0))
 	image = create("Image", lambda _,m: bool(m.contentType == 1))
@@ -66,32 +69,55 @@ class Filters:
 	event = create("Event", lambda _,m: bool(m.contentType == 18))
 	music = create("Music", lambda _,m: bool(m.contentType == 19))
 	mention = create("Mention", lambda _,m: bool('MENTION' in m.contentMetadata.keys()))
-	reply = create("Reply", lambda _,m: bool(m.contentMetadata["message_relation_type_code"] == "reply" and m.contentMetadata != {}))
-	forward = create("Forward", lambda _,m: bool(m.contentMetadata["message_relation_type_code"] == "forward" and m.contentMetadata != {}))
-
+	reply = create("Reply", lambda _,m: bool("reply" in m.contentMetadata.values()))
+	#TODO: Forward only worked for text type, i cant found clue to catch forward media as image,video,audio
+	forward = create("Forward", lambda _,m: bool("forward" in m.contentMetadata.values()))
+	
 	#Instance
 	group = create("Group", lambda _,m: bool(m.toType == 2))
 	private = create("Private", lambda _,m: bool(m.toType == 0))
 	both = create("Both", lambda _,m: bool(m.toType in [0, 2, 1]))
-
+	
 	#update group
 	update_name = create("UpdateName", lambda _,m: bool(m.param3 == '1'))
 	update_image = create("UpdateImage", lambda _,m: bool(m.param3 == '2'))
 	update_qr = create("UpdateQr", lambda _,m: bool(m.param3 == '4'))
 	update_all = create("UpdateAll", lambda _,m: bool(m.param3 in ["1", "2", "4"]))
-
+	
 	@staticmethod
 	def command(commands: str or list,
 					prefix: str or list = "/",
 					separator: str = " ",
 					case_sensitive: bool = True):
+		"""Filter commands, i.e.: text messages starting with "/" or any other custom prefix.
+
+        Args:
+            command (``str`` | ``list``):
+                The command or list of commands as string the filter should look for.
+                Examples: "start", ["start", "help", "settings"]. When a message text containing
+                a command arrives, the command itself and its arguments will be stored in the *command*
+                field of the :class:`Message <akad.ttypes.Message>`.
+
+            prefix (``str`` | ``list``, *optional*):
+                A prefix or a list of prefixes as string the filter should look for.
+                Defaults to "/" (slash). Examples: ".", "!", ["/", "!", "."].
+                Can be None or "" (empty string) to allow commands with no prefix at all.
+
+            separator (``str``, *optional*):
+                The command arguments separator. Defaults to " " (white space).
+                Examples: /start first second, /start-first-second, /start.first.second.
+
+            case_sensitive (``bool``, *optional*):
+                Pass True if you want your command(s) to be case sensitive. Defaults to False.
+                Examples: when True, command="Start" would trigger /Start but not /start.
+        """
 		def f(_, m):
 			m.command = False
 			if m.text:
 				for i in _.p:
 					if m.text.startswith(i):
 						t = m.text.split(_.s)
-						c, a = t[0][len(i):], t[1:]
+						c, a = t[0][len(i):], t[1:]												
 						c = c if _.cs else c.lower()
 						m.command = ([c] + a) if c in _.c else None						
 			return bool(m.command)
@@ -101,23 +127,51 @@ class Filters:
 		c = {commands if case_sensitive
 				else commands.lower()}
 		if not isinstance(commands, list)
-		else {c if case_sensitive
+		else {c if case_sensitive				
 				else c.lower()
 				for c in commands},
 		p=set(prefix) if prefix else {""},
 		s=separator,
 		cs=case_sensitive
 		)
-
+	
+	@staticmethod
+	def regex(pattern, flags: int = 0):
+		"""Filter messages that match a given RegEx pattern.
+		
+		Args:
+			pattern (``str``):
+				The RegEx pattern as string, it will be applied to the text of a message. When a pattern matches,
+				all the `Match Objects <https://docs.python.org/3/library/re.html#match-objects>`
+			
+			flags (``int``, *optional*):
+				RegEx flags.
+		"""
+		
+		def f(_, m):		
+			m.matches = [i for i in _.p.finditer(m.text or "")]
+			return bool(m.matches)
+		return create("Regex", f, p=re.compile(pattern, flags))
+        	
 	class user(Filter, set):
+		"""Filter messages coming from one or more users.
+		
+		You can use `set bound methods <https://docs.python.org/3/library/stdtypes.html#set>`_ to manipulate the
+		users container.
+
+        Args:
+            users (``str`` | ``list``):
+                Pass one or more user mid to filter users.
+                Defaults to None (no users).
+        """
 		def __init__(self, users: int or str or list = None):
 			users = [] if users is None else users if isinstance(users, list) else [users]
 			super().__init__(
-				{"me" if i in ["me", "self"] else i.lower() if isinstance(i, str)  else i for i in users}
+				{"me" if i in ["me", "self"] else i.lower() if isinstance(i, str) else i for i in users}
 				if isinstance(users, list) else
-				{"me" if users in ["me", "self"] else users.lower() if isinstance(i, str) else users}
+				{"me" if users in ["me", "self"] else users.lower() if isinstance(users, str) else user}
 			)
-
+			
 		def __call__(self, message):
 			return bool(
 				message._from
@@ -125,14 +179,228 @@ class Filters:
 					or ("me" in self)
 				)
 			)
-
+			
 	class chat(Filter, set):
+		"""Filter messages coming from one or more chats.
+		
+		You can use `set bound methods <https://docs.python.org/3/library/stdtypes.html#set>`_ to manipulate the
+		chats container.
+
+        Args:
+            chats (``str`` | ``list``):
+                Pass one or more chat mid to filter chats.
+                Defaults to None (no chats).
+        """
 		def __init__(self, chats: int or str or list = None):
 			chats = [] if chats is None else chats if isinstance(chats, list) else [chats]
 			super().__init__(
-                {i.lower() if isinstance(i, str)  else i for i in chats}
+                {i.lower() if isinstance(i, str) else i for i in chats}
                 if isinstance(chats, list) else
-                {chats.lower() if isinstance(chats, str) else chats}
+                {chats.lower() if isinstance(chats, list) else chats}
+            )
+		def __call__(self, message):
+			return bool(
+				message.toType == 2
+				and (message.to in self)
+			)import re
+
+class Filter:
+    def __call__(self, message):
+        raise NotImplementedError
+
+    def __invert__(self):
+        return InvertFilter(self)
+
+    def __and__(self, other):
+        return AndFilter(self, other)
+
+    def __or__(self, other):
+        return OrFilter(self, other)
+
+
+class InvertFilter(Filter):
+    def __init__(self, base):
+        self.base = base
+
+    def __call__(self, message):
+        return not self.base(message)
+
+
+class AndFilter(Filter):
+    def __init__(self, base, other):
+        self.base = base
+        self.other = other
+
+    def __call__(self, message):
+        return self.base(message) and self.other(message)
+
+
+class OrFilter(Filter):
+    def __init__(self, base, other):
+        self.base = base
+        self.other = other
+
+    def __call__(self, message):
+        return self.base(message) or self.other(message)
+
+
+def create(name: str, func: callable, **kwargs) -> type:
+	d = {"__call__": func}
+	d.update(kwargs)
+	
+	return type(name, (Filter,), d)()
+    
+class Filters:
+	
+	create = create
+	
+	#content
+	text = create("Text", lambda _,m: bool(m.contentType == 0))
+	image = create("Image", lambda _,m: bool(m.contentType == 1))
+	video = create("Video", lambda _,m: bool(m.contentType == 2))
+	audio = create("Audio", lambda _,m: bool(m.contentType == 3))
+	html = create("Html", lambda _,m: bool(m.contentType == 4))
+	pdf = create("Pdf", lambda _,m: bool(m.contentType == 5))
+	call = create("Call", lambda _,m: bool(m.contentType == 6))
+	sticker = create("Sticker", lambda _,m: bool(m.contentType == 7))
+	gift = create("Gift", lambda _,m: bool(m.contentType == 9))
+	link = create("Link", lambda _,m: bool(m.contentType == 12))
+	contact = create("Contact", lambda _,m: bool(m.contentType == 13))
+	files = create("Files", lambda _,m: bool(m.contentType == 14))
+	location = create("Location", lambda _,m: bool(m.contentType == 15))
+	post = create("Post", lambda _,m: bool(m.contentType == 16))
+	rich = create("Rich", lambda _,m: bool(m.contentType == 17))
+	event = create("Event", lambda _,m: bool(m.contentType == 18))
+	music = create("Music", lambda _,m: bool(m.contentType == 19))
+	mention = create("Mention", lambda _,m: bool('MENTION' in m.contentMetadata.keys()))
+	reply = create("Reply", lambda _,m: bool("reply" in m.contentMetadata.values()))
+	#TODO: Forward only worked for text type, i cant found clue to catch forward media as image,video,audio
+	forward = create("Forward", lambda _,m: bool("forward" in m.contentMetadata.values()))
+	
+	#Instance
+	group = create("Group", lambda _,m: bool(m.toType == 2))
+	private = create("Private", lambda _,m: bool(m.toType == 0))
+	both = create("Both", lambda _,m: bool(m.toType in [0, 2, 1]))
+	
+	#update group
+	update_name = create("UpdateName", lambda _,m: bool(m.param3 == '1'))
+	update_image = create("UpdateImage", lambda _,m: bool(m.param3 == '2'))
+	update_qr = create("UpdateQr", lambda _,m: bool(m.param3 == '4'))
+	update_all = create("UpdateAll", lambda _,m: bool(m.param3 in ["1", "2", "4"]))
+	
+	@staticmethod
+	def command(commands: str or list,
+					prefix: str or list = "/",
+					separator: str = " ",
+					case_sensitive: bool = True):
+		"""Filter commands, i.e.: text messages starting with "/" or any other custom prefix.
+
+        Args:
+            command (``str`` | ``list``):
+                The command or list of commands as string the filter should look for.
+                Examples: "start", ["start", "help", "settings"]. When a message text containing
+                a command arrives, the command itself and its arguments will be stored in the *command*
+                field of the :class:`Message <akad.ttypes.Message>`.
+
+            prefix (``str`` | ``list``, *optional*):
+                A prefix or a list of prefixes as string the filter should look for.
+                Defaults to "/" (slash). Examples: ".", "!", ["/", "!", "."].
+                Can be None or "" (empty string) to allow commands with no prefix at all.
+
+            separator (``str``, *optional*):
+                The command arguments separator. Defaults to " " (white space).
+                Examples: /start first second, /start-first-second, /start.first.second.
+
+            case_sensitive (``bool``, *optional*):
+                Pass True if you want your command(s) to be case sensitive. Defaults to False.
+                Examples: when True, command="Start" would trigger /Start but not /start.
+        """
+		def f(_, m):
+			m.command = False
+			if m.text:
+				for i in _.p:
+					if m.text.startswith(i):
+						t = m.text.split(_.s)
+						c, a = t[0][len(i):], t[1:]												
+						c = c if _.cs else c.lower()
+						m.command = ([c] + a) if c in _.c else None						
+			return bool(m.command)
+		return create(
+		"Command",
+		f,
+		c = {commands if case_sensitive
+				else commands.lower()}
+		if not isinstance(commands, list)
+		else {c if case_sensitive				
+				else c.lower()
+				for c in commands},
+		p=set(prefix) if prefix else {""},
+		s=separator,
+		cs=case_sensitive
+		)
+	
+	@staticmethod
+	def regex(pattern, flags: int = 0):
+		"""Filter messages that match a given RegEx pattern.
+		
+		Args:
+			pattern (``str``):
+				The RegEx pattern as string, it will be applied to the text of a message. When a pattern matches,
+				all the `Match Objects <https://docs.python.org/3/library/re.html#match-objects>`
+			
+			flags (``int``, *optional*):
+				RegEx flags.
+		"""
+		
+		def f(_, m):		
+			m.matches = [i for i in _.p.finditer(m.text or "")]
+			return bool(m.matches)
+		return create("Regex", f, p=re.compile(pattern, flags))
+        	
+	class user(Filter, set):
+		"""Filter messages coming from one or more users.
+		
+		You can use `set bound methods <https://docs.python.org/3/library/stdtypes.html#set>`_ to manipulate the
+		users container.
+
+        Args:
+            users (``str`` | ``list``):
+                Pass one or more user mid to filter users.
+                Defaults to None (no users).
+        """
+		def __init__(self, users: int or str or list = None):
+			users = [] if users is None else users if isinstance(users, list) else [users]
+			super().__init__(
+				{"me" if i in ["me", "self"] else i.lower() if isinstance(i, str) else i for i in users}
+				if isinstance(users, list) else
+				{"me" if users in ["me", "self"] else users.lower() if isinstance(users, str) else user}
+			)
+			
+		def __call__(self, message):
+			return bool(
+				message._from
+				and (message._from in self
+					or ("me" in self)
+				)
+			)
+			
+	class chat(Filter, set):
+		"""Filter messages coming from one or more chats.
+		
+		You can use `set bound methods <https://docs.python.org/3/library/stdtypes.html#set>`_ to manipulate the
+		chats container.
+
+        Args:
+            chats (``str`` | ``list``):
+                Pass one or more chat mid to filter chats.
+                Defaults to None (no chats).
+        """
+		def __init__(self, chats: int or str or list = None):
+			chats = [] if chats is None else chats if isinstance(chats, list) else [chats]
+			super().__init__(
+                {i.lower() if isinstance(i, str) else i for i in chats}
+                if isinstance(chats, list) else
+                {chats.lower() if isinstance(chats, list) else chats}
             )
 		def __call__(self, message):
 			return bool(
