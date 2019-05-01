6718f640729f3655ff3283ba84ca5bf9dc1e03b4
Alnyz <katro.coplax@gmail.com>
2019-05-01 10:51:53 +0700

    bump up request preform

--- linepy/server.py
+++ linepy/server.py
@@ -1,68 +1,71 @@
 # -*- coding: utf-8 -*-
 from .config import Config
 import json, requests, urllib
+from hyper import HTTPConnection
 
 class Server(Config):
-    _session        = requests.session()
-    timelineHeaders = {}
-    Headers         = {}
+	def __init__(self, host=None, appType=None):
+		self.reqs = requests.Session()
+		self._session = HTTPConnection(host.replace("https://",""))
+		self.timelineHeaders = {}
+		self.Headers = {}
+		Config.__init__(self, appType)
 
-    def __init__(self, appType=None):
-        self.Headers = {}
-        self.timelineHeaders = {}
-        Config.__init__(self, appType)
+	def parseUrl(self, path):
+		return self.LINE_HOST_DOMAIN + path
 
-    def parseUrl(self, path):
-        return self.LINE_HOST_DOMAIN + path
+	def urlEncode(self, url, path, params=[]):
+		return url + path + '?' + urllib.parse.urlencode(params)
 
-    def urlEncode(self, url, path, params=None):
-        return url + path + '?' + urllib.parse.urlencode(params)
+	def getJson(self, url, allowHeader=False):		
+		if not allowHeader:
+			res = self.reqs.get(url, timeout=None)
+		else:
+			res = self.reqs.get(url, headers=self.Headers, timeout=None)
+		return json.loads(res.text)
 
-    def getJson(self, url, allowHeader=False):
-        if allowHeader is False:
-            return json.loads(self._session.get(url).text)
-        else:
-            return json.loads(self._session.get(url, headers=self.Headers).text)
+	def setHeadersWithDict(self, headersDict):
+		self.Headers.update(headersDict)
 
-    def setHeadersWithDict(self, headersDict):
-        self.Headers.update(headersDict)
+	def setHeaders(self, argument, value):
+		self.Headers[argument] = value
 
-    def setHeaders(self, argument, value):
-        self.Headers[argument] = value
+	def setTimelineHeadersWithDict(self, headersDict):
+		self.timelineHeaders.update(headersDict)
 
-    def setTimelineHeadersWithDict(self, headersDict):
-        self.timelineHeaders.update(headersDict)
+	def setTimelineHeaders(self, argument, value):
+		self.timelineHeaders[argument] = value
 
-    def setTimelineHeaders(self, argument, value):
-        self.timelineHeaders[argument] = value
+	def additionalHeaders(self, source, newSource):
+		headerList={}
+		headerList.update(source)
+		headerList.update(newSource)
+		return headerList
 
-    def additionalHeaders(self, source, newSource):
-        headerList={}
-        headerList.update(source)
-        headerList.update(newSource)
-        return headerList
+	def optionsContent(self, url, data=None, headers=None):
+		if headers is None:
+			headers=self.Headers
+		return self.request("OPTIONS", url, headers=headers, data=data)
 
-    def optionsContent(self, url, data=None, headers=None):
-        if headers is None:
-            headers=self.Headers
-        return self._session.options(url, headers=headers, data=data)
+	def postContent(self, url, data=None, files=None, headers=None):
+		if headers is None:
+			headers=self.Headers
+		return self.request("POST", url, headers=headers, data=data, files=files)
 
-    def postContent(self, url, data=None, files=None, headers=None):
-        if headers is None:
-            headers=self.Headers
-        return self._session.post(url, headers=headers, data=data, files=files)
+	def getContent(self, url, headers=None):
+		if headers is None:
+			headers=self.Headers
+		return self.request("GET", url, headers=headers)
 
-    def getContent(self, url, headers=None):
-        if headers is None:
-            headers=self.Headers
-        return self._session.get(url, headers=headers, stream=True)
+	def deleteContent(self, url, data=None, headers=None):
+		if headers is None:
+			headers=self.Headers
+		return self.request("DELETE", url, headers=headers, data=data)
 
-    def deleteContent(self, url, data=None, headers=None):
-        if headers is None:
-            headers=self.Headers
-        return self._session.delete(url, headers=headers, data=data)
+	def putContent(self, url, data=None, headers=None):
+		if headers is None:
+			headers=self.Headers
+		return self.request("PUT", url, headers=headers, data=data)
 
-    def putContent(self, url, data=None, headers=None):
-        if headers is None:
-            headers=self.Headers
-        return self._session.put(url, headers=headers, data=data)
+	def request(self,method, url, data=None, headers=None):
+		self._session.request(method.upper(), url, body=data, headers=headers)
